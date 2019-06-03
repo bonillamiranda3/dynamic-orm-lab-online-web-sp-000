ce3e763ff9380a1c29b757f61517424d2e7df538
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-06-03 05:15:14 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-    def initialize(options{})
+    def initialize(options={})
       options.each do |property, value|
         self.send("#{property}=", value)
     end
