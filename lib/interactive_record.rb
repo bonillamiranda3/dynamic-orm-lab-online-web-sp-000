8add46a44c5928805456baacee5b6302ffe34382
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-06-03 05:39:15 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -41,21 +41,21 @@ class InteractiveRecord
     end
 
     def save
-     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-     DB[:conn].execute(sql)
-     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-   end
+  sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+  DB[:conn].execute(sql)
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+ end
 
-   def self.find_by_name(name)
-     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-     DB[:conn].execute(sql)
-   end
+ def self.find_by_name(name)
+   sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+   DB[:conn].execute(sql)
+ end
 
-   def self.find_by(hash)
-     find = hash.collect do |key,attribute|
-       sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{attribute}'"
-       DB[:conn].execute(sql)
-     end
-     find[0]
-   end
+ def self.find_by(attribute)
+   sql = <<-SQL
+     SELECT * FROM #{self.table_name}
+     WHERE #{attribute.keys.first.to_s} = '#{attribute.values.first.to_s}'
+   SQL
+   DB[:conn].execute(sql)
+ end
 end
